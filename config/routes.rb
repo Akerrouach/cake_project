72112c581c8c725d3ddff25e71f528fc05b32a5d
Kevin Buhler <buhler.kevin@ymail.com>
2015-04-29 17:56:59 +0200

    add edit and delete to shops

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :pastries, only: [:index]
 
-  resources :shops, only: [:show, :new, :create, :edit, :update, :destroy] do
+  resources :shops, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :pastries, except: [:index, :show]
   end
 
