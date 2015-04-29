ddc5bf3e35dcb8974f3a99cc6b625a64bca51d73
Fanny Roseau <fannyroseau@hotmail.com>
2015-04-29 11:18:26 +0200

    fix home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
- # root to: "home#index"
-
  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   resources :users, only: [:show]
