94e51662d124f0c884c7bc71904eca8b49c8ec6a
Kevin Buhler <buhler.kevin@ymail.com>
2015-05-04 19:42:46 +0200

    change time with i18n for created_at

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504092113) do
+ActiveRecord::Schema.define(version: 20150504161518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,12 +61,14 @@ ActiveRecord::Schema.define(version: 20150504092113) do
   end
 
   create_table "shopping_carts", force: :cascade do |t|
-    t.float   "total_price"
-    t.boolean "status"
-    t.date    "delivery_date"
-    t.integer "user_id"
-    t.integer "shop_id"
-    t.boolean "accepted"
+    t.float    "total_price"
+    t.boolean  "status"
+    t.date     "delivery_date"
+    t.integer  "user_id"
+    t.integer  "shop_id"
+    t.boolean  "accepted"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "shopping_carts", ["user_id"], name: "index_shopping_carts_on_user_id", using: :btree
