9969120edd66807d9757af60fdb47461397fac06
Kevin Buhler <buhler.kevin@ymail.com>
2015-05-05 19:43:09 +0200

    fix bug fbconnect name

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,8 @@ class User < ActiveRecord::Base
         user.uid = auth.uid
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]  # Fake password for validation
-        user.name = auth.info.name
+        user.first_name = auth.info.name.split[0]
+        user.last_name = auth.info.name.split[1]
         user.picture = auth.info.image
         user.token = auth.credentials.token
         user.token_expiry = Time.at(auth.credentials.expires_at)
