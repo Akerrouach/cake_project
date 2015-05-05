b31fc3f08911dea1b08e6d3fac9ef6f4ac26f9c8
Ake Anouar <a.akerrouach@gmail.com>
2015-05-05 19:47:10 +0200

    fix fbconnect name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,40 +1,40 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  has_many :shops, dependent: :destroy
-  has_many :shopping_carts
+ # Include default devise modules. Others available are:
+ has_many :shops, dependent: :destroy
+ has_many :shopping_carts
 
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :address, presence: true
-  # validates :city, presence: true
-  # validates :zip_code, presence: true
-  # validates :phone_number, presence: true
+ # validates :first_name, presence: true
+ # validates :last_name, presence: true
+ # validates :address, presence: true
+ # validates :city, presence: true
+ # validates :zip_code, presence: true
+ # validates :phone_number, presence: true
 
 
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [ :facebook ]
+ # :confirmable, :lockable, :timeoutable and :omniauthable
+ devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable,
+        :omniauthable, :omniauth_providers => [ :facebook ]
 
-  has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
+ has_attached_file :picture,
+   styles: { medium: "300x300>", thumb: "100x100>" }
 
-  validates_attachment_content_type :picture,
-    content_type: /\Aimage\/.*\z/
+ validates_attachment_content_type :picture,
+   content_type: /\Aimage\/.*\z/
 
-  def self.find_for_facebook_oauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0,20]  # Fake password for validation
-        user.name = auth.info.name
-        user.picture = auth.info.image
-        user.token = auth.credentials.token
-        user.token_expiry = Time.at(auth.credentials.expires_at)
-      end
-  end
-
+ def self.find_for_facebook_oauth(auth)
+     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+       user.provider = auth.provider
+       user.uid = auth.uid
+       user.email = auth.info.email
+       user.password = Devise.friendly_token[0,20]  # Fake password for validation
+       user.first_name = auth.info.name.split[0]
+       user.last_name = auth.info.name.split[1]
+       user.picture = auth.info.image
+       user.token = auth.credentials.token
+       user.token_expiry = Time.at(auth.credentials.expires_at)
+     end
+ end
 
 
 end
